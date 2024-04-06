87436a61a12a739caedd4fa8c3a8ee98717e4b80
Saskia <131782929+SaskiaJuergens@users.noreply.github.com>
2024-04-06 23:20:27 +0200

    first Firebase try
    Add, Retriev, Remove, Update im "firstWebsite" Odner funktioniert

---
+++ js/tableFunction.js
@@ -0,0 +1,20 @@
+var name; //konstante für Hover und vorschlagen von Teilnehmern
+var vorname;
+var geburstag;
+var alter;
+var graduierung; //drop down
+var geschlecht; //drop down
+var nationalität;
+var verein;
+
+var playerData = []; //Eventhandler für HTML
+
+// Beim Laden der Webseite wird die Funktion setup() aufgerufen
+window.addEventListener("load", setup);
+function setup() {
+    showTeilnehmer();
+}
+
+function showTeilnehmer(){
+
+}
